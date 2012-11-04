bd057314431fdc83f3225f6ea0612a6eb46ae28d
nitram509 <maki@bitkings.de>
2012-11-05 00:34:08 +0100

    reworked binary search (see wikipedia) and FIXed last 'red' test

--- geoip.js
+++ geoip.js
@@ -21,62 +21,33 @@ module.exports = geoip = {
  */
 function find(ip) {
 
-   var mpi = 0;
-   var n = midpoints[0];
-   var step;
    var parts = ip.split(".");
    var target_ip = parseInt(parts[3], 10) +
-                  (parseInt(parts[2], 10) * 256) +
-                  (parseInt(parts[1], 10) * 65536) +
-                  (parseInt(parts[0], 10) * 16777216);
+      (parseInt(parts[2], 10) * 256) +
+      (parseInt(parts[1], 10) * 65536) +
+      (parseInt(parts[0], 10) * 16777216);
 
-  var current;
-  var next;
-  var prev;
-  var nn;
-  var pn;
-
-  while(true) {
-
-      mpi++;
-      step = midpoints[mpi];
-      current = countries[n];
-      nn = n + 1;
-      pn = n - 1;
-
-      next = nn < numcountries ? countries[nn] : null;
-      prev = pn > -1 ? countries[pn] : null;
-
-      // take another step?
-      if(step > 0) {
-
-          if(!next || next.ipstart < target_ip) {
-              n += step;
-          } else {
-              n -= step;
-          }
-
-          continue;
+   var idxMin = 0;
+   var idxMiddle = 0;
+   var idxMax = numcountries - 1;
+   var pickedCountry = undefined;
+   while (idxMin < idxMax) {
+      idxMiddle = (idxMax + idxMin) >> 1;
+      pickedCountry = countries[idxMiddle];
+      // determine which subarray to search
+      if (pickedCountry.ipstart < target_ip) {
+         // change min index to search upper subarray
+         idxMin = idxMiddle + 1;
+      } else if (pickedCountry.ipstart > target_ip) {
+         // change max index to search lower subarray
+         idxMax = idxMiddle - 1;
+      } else {
+         // key found at index imid
+         return pickedCountry;
       }
-
-      // we're either current, next or previous depending on which is closest to target_ip
-      var curr_ip_diff = Math.abs(target_ip - current.ipstart);
-      var next_ip_diff = next && next.ipstart < target_ip ? target_ip - next.ipstart : 1000000000;
-      var prev_ip_diff = prev && prev.ipstart <= target_ip ? target_ip - prev.ipstart : 1000000000;
-
-      // current wins
-      if(curr_ip_diff < next_ip_diff && curr_ip_diff < prev_ip_diff) {
-          return current;
-      }
-
-      // next wins
-      if(next_ip_diff < curr_ip_diff && next_ip_diff < prev_ip_diff) {
-          return next;
-      }
-
-      // prev wins
-      return prev;
-    }
+   }
+   // return previous found country.
+   return pickedCountry;
 }
 
 /**
